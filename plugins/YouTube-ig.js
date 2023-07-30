f0488bb703692618bcb652800ecb44348fca4351
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-07-30 13:28:28 -0600

    Actualizar YouTube-ig.js

--- plugins/YouTube-ig.js
+++ plugins/YouTube-ig.js
@@ -5,7 +5,7 @@ import instagramGetUrl from 'instagram-url-direct'
 import vm from 'node:vm'
 import { instagramdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link\n\n\n*[❗𝐈𝐍𝐅𝐎❗]  ENTER AN INSTAGRAM LINK, EXAMPLE*:${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*` 
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link\n\n\n*[❗𝐈𝐍𝐅𝐎❗]  ENTER AN INSTAGRAM LINK, EXAMPLE*:${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuR/?utm_source=ig_web_copy_link*` 
 m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*\n\n\n*[❗]DOWNLOADING YOUR VIDEO, PLEASE WAIT A MOMENT, THE PROCESS WILL LAST BETWEEN 2 AND 10 MINUTES DEPENDING ON THE VIDEO...*`)
 try {
 const datTa = await instagram.v1(args[0])
