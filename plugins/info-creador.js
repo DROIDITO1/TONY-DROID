e0733ebce346d88aff08c21b46d5b5ee0267d28c
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-07-30 12:56:44 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,29 +2,29 @@ let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let text = `╭─「➻❥ *owner* ➻❥」
-│➯ ⫷᭄©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰﹏✍
-│➯Wa.me/5212412377467 (CREADOR)
-│➯Wa.me/5212411261883 (CREADOR) 
-│➯Wa.me/5212411719888 (CREADOR)
-│➯Wa.me/5212411347465 (CREADOR)
+│➯ ⫷᭄©𝐃𝐑𝐎𝐈𝐃-8-𝐌𝐃﹏✍
+│➯Wa.me/50258487658 (CREADOR)
+│➯Wa.me/50258487658 (CREADOR) 
+│➯Wa.me/50258487658 (CREADOR)
+│➯Wa.me/50258487658 (CREADOR)
 ╰───────────────╯`.trim()   
 let buttonMessage= {
-'document': { url: `https://github.com/OFC-YOVANI/HADES-BOT-OMEGA` },
+'document': { url: `https://chat.whatsapp.com/HCJozD71O1VBtW68E5hu4L` },
 'mimetype': `application/${document}`,
-'fileName': `⫷᭄ʜᴀᴅᴇs-ʙᴏᴛ-ᴍᴅ﹏✍`,
+'fileName': `⫷᭄𝐃𝐑𝐎𝐈𝐃-8-𝐌𝐃﹏✍`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'https://github.com/OFC-YOVANI/HADES-BOT-OMEGA',
+'mediaUrl': 'https://chat.whatsapp.com/HCJozD71O1VBtW68E5hu4L',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
 'body': wm,
 'thumbnail': imagen1,
-'sourceUrl': 'https://youtu.be/4eA5nDxPjOA' }},
+'sourceUrl': 'http://tiktok.com/@coin.exe1' }},
 'caption': text,
 'footer': wm,
 //'buttons':[
