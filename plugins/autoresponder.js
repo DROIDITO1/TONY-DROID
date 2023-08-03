35d626860696d31a674ec241634da5e28ee8c466
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-02 21:50:47 -0600

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '⫷᭄©𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰﹏✍', body: '©ᴏғᴄ-ʏᴏᴠᴀɴɪ﹏✍', sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '⫷᭄©𝐃𝐑𝐎𝐈𝐃-8-𝐌𝐃﹏✍', body: '©ᴏғᴄ-ᴛᴏɴʏ﹏✍', sourceUrl: `http://tiktok.com/@coin.exe1`, thumbnail: imagen2}}})}
     
 return !0 }
 handler.register = true
