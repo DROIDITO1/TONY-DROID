1f569e984013098889b434814b807be245f47d6e
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-03 22:25:52 -0600

    Actualizar random-Messi.js

--- plugins/random-Messi.js
+++ plugins/random-Messi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/OFC-YOVANI/HADES-BOT-MD/master/src/hades/Messi.json`)).data  
+let res = (await axios.get(`http://tiktok.com/@coin.exe1/master/src/hades/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
