bb92124eb59f23650faaf2c67f3b517b0fbca29c
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-17 10:08:04 -0600

    Actualizar Sticker-sticker.js

--- plugins/Sticker-sticker.js
+++ plugins/Sticker-sticker.js
@@ -4,48 +4,49 @@ import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-let stiker = false
-try {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] EL VIDEO NO PUEDE DURAR MÁS DE 7 SEGUNDOS*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]THE VIDEO CANNOT BE LONGER THAN 7 SECONDS*')
-let img = await q.download?.()
-
-if (!img) throw `*[❗𝐈𝐍𝐅𝐎❗] RESPONDA UN VIDEO, IMAGEN O INSERTE EL ENLACE DE UNA IMAGEN TERMINACIÓN.jpg EL CUÁL SERÁ CONVERTIDO EN STICKER, DEBE RESPONDER O USAR EL COMANDO ${usedPrefix + command}*\n\n\n*[❗𝐈𝐍𝐅𝐎❗] REPLY TO A VIDEO, OR IMAGE OF THE LINK OF AN IMAGE ENDING .jpg IN WHICH IT WILL BE CONVERTED INTO STICKERS, YOU MUST RESPOND USING THE COMMAND ${usedPrefix + command}*`
-
-let out
-try {
-stiker = await sticker(img, false, global.packname, global.author)
-} catch (e) {
-console.error(e)
-} finally {
-if (!stiker) {
-if (/webp/g.test(mime)) out = await webp2png(img)
-else if (/image/g.test(mime)) out = await uploadImage(img)
-else if (/video/g.test(mime)) out = await uploadFile(img)
-if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, global.packname, global.author)
-}}
-} else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-
-else return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙽𝙾 𝙴𝚂 𝚅𝙰𝙻𝙸𝙳𝙰, 𝙻𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 .𝚓𝚙𝚐, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: #s https://telegra.ph/file/0dc687c61410765e98de2.jpg*\n\n\n[❗𝐈𝐍𝐅𝐎❗]   THE LINK /UEL /LINK IS NOT VALIDATED, THE END OF THE LINK /URL /LINK / SHOULD BE .jpg, EXAMPLE #s https://telegra.ph/file/0dc687c61410765e98de2.jpg*')
-  
+  let stiker = false
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+  try { 	
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/webp|image|video/g.test(mime)) {
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('🧑🏻‍💻 Máximo 10 segundos')
+      let img = await q.download?.()
+      if (!img) throw `🧑🏻‍💻️ Responde a una imagen o video con${usedPrefix + command}`
+      let out
+      try {
+        stiker = await sticker(img, false, f, g)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          if (/webp/g.test(mime)) out = await webp2png(img)
+          else if (/image/g.test(mime)) out = await uploadImage(img)
+          else if (/video/g.test(mime)) out = await uploadFile(img)
+          if (typeof out !== 'string') out = await uploadImage(img)
+          stiker = await sticker(false, out, f, g)
+        }
+      }
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL invalido')
+    }
+  } catch (e) {
+    console.error(e)
+    if (!stiker) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, rpl)
+    else throw '🧑🏻‍💻 La conversión ha fallado, intenta enviar primero imagen/video/gif y luego responde con el comando'
+  }
 }
-} catch (e) {
-console.error(e)
-if (!stiker) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-
-else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾. 𝙽𝙾 𝙾𝙻𝚅𝙸𝙳𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]SORRY, I MADE AN ERROR, TRY AGAIN. DO NOT FORGET TO RESPOND TO A VIDEO IMAGE OR INSERT THE LINK OF AN IMAGE FINISHING .jpg OF WHICH IT WILL BE CONVERTED INTO A STICKER* '
-}}
-handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
+handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
-handler.register = true
-handler.limit = 1
+handler.command = ['s', 'sticker'] 
+
 export default handler
+
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
