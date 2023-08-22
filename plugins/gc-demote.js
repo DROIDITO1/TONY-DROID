19f014db0f7ffc8d80938048a09c4da39b70a3df
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-21 22:11:09 -0600

    Actualizar gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -21,7 +21,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `*[ ✅ ] ÓRDENES RECIBIDAS*`, m)
+conn.reply(m.chat, `*👨🏻‍💻 ÓRDENES RECIBIDAS*`, m)
 }}
 handler.help = ['*593xxx*','*@usuario*','*responder chat*'].map(v => 'demote ' + v)
 handler.tags = ['group']
