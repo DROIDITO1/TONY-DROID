6fe6e3379bdd704cc7beee25c4e4cd9f6e32bfe8
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-22 09:44:29 -0600

    Actualizar fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 conn.reply(m.chat, `
-🤤👅🥵 *𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!*🥵👅🤤
+🤤👅🥵 *ACABAS DE FOLLARTEL@!*🥵👅🤤
 
-*𝙏𝙚 𝙖𝙘𝙖𝙗𝙖𝙨 𝙙𝙚 𝙛𝙤𝙡𝙡𝙖𝙧 𝙖 𝙡𝙖 𝙥𝙚𝙧𝙧𝙖 𝙙𝙚* *${text}* ⁩ *𝙖 𝟰 𝙥𝙖𝙩𝙖𝙨 𝙢𝙞𝙚𝙣𝙩𝙧𝙖𝙨 𝙩𝙚 𝙜𝙚𝙢𝙞𝙖 𝙘𝙤𝙢𝙤 𝙪𝙣𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙥𝙚𝙧𝙧𝙖 "𝐀𝐚𝐚𝐡.., 𝐀𝐚𝐚𝐡𝐡, 𝐬𝐢𝐠𝐮𝐞, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬.." 𝙮 𝙡𝙖 𝙝𝙖𝙨 𝙙𝙚𝙟𝙖𝙙𝙤 𝙩𝙖𝙣 𝙧𝙚𝙫𝙚𝙣𝙩𝙖𝙙𝙖 𝙦𝙪𝙚 𝙣𝙤 𝙥𝙪𝙚𝙙𝙚 𝙨𝙤𝙨𝙩𝙚𝙣𝙚𝙧 𝙣𝙞 𝙨𝙪 𝙥𝙧𝙤𝙥𝙞𝙤 𝙘𝙪𝙚𝙧𝙥𝙤 𝙡𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙯𝙤𝙧𝙧𝙖!*
+*TE ACABAS DE FOLLAR A LA PERRA DE* *${text}* ⁩ *A 4 PATAS MIENTRAS TE GEMIA COMO UNA MALDITA PERRA "Aaah.., Aaah, SIGUE, no pares, no pares.." Y LAS HAS DEJAD@ TAN REVENTADA QUE NO PUEDE SOSTENER NU SU PROPIO CUERPO LA MALDITA ZORRA!*
 
 *${text}* 
-🤤🥵 *¡𝐘𝐀 𝐓𝐄 𝐇𝐀𝐍 𝐅𝐎𝐋𝐋𝐀𝐃𝐎!* 🥵🤤`, null, { mentions: [user] })
+🤤🥵 *¡YA TE HAN FOLLADO!* 🥵🤤`, null, { mentions: [user] })
 }
 
 handler.command = /^(Follar|violar)/i
