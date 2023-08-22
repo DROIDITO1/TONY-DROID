6e40eb8adb01789fdbd2a2c536f272ccc727fe69
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-21 22:29:23 -0600

    Actualizar fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,14 +1,24 @@
-let handler = async (m, { conn }) => { 
-   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender 
-   let name = conn.getName(who) 
-   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', { 
-     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'),  
-   }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nQuién quiere violar a este gay? `, m) 
- } 
-  
- handler.help = ['gay *@user*'] 
- handler.tags = ['fun'] 
- handler.command = ['gay']  
- handler.register = true 
-  
- export default handler
+```javascript
+let handler = async (m, { conn }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let name = conn.getName(who);
+
+    try {
+        let response = await axios.get(`https://some-random-api.ml/canvas/gay?avatar=${encodeURIComponent(await conn.getProfilePicture(who))}`);
+        if (response.data && response.data.link) {
+            conn.sendFile(m.chat, response.data.link, 'gay.png', `🏳️‍🌈 *Gay:* ${name}\n\n¡¿Quién quiere violar a este gay?!`, m);
+        } else {
+            conn.reply(m.chat, 'No se pudo aplicar el filtro gay a la imagen.', m);
+        }
+    } catch (e) {
+        conn.reply(m.chat, 'Ocurrió un error al procesar la solicitud.', m);
+    }
+}
+
+handler.help = ['gay *@user*'];
+handler.tags = ['fun'];
+handler.command = ['gay'];
+handler.register = true;
+
+export default handler;
+```
