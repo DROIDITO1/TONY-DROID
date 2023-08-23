b6fe4dfab7e354aa7e6d64337c7ae689f47f4828
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-23 10:51:46 -0600

    Actualizar rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -4,20 +4,17 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
 m.reply(`╭「➻❥𝐃𝐑𝐎𝐈𝐃-8-𝐌𝐃➻❥」
-│💰 *𝐁𝐀𝐋𝐀𝐍𝐂𝐄* 
-│┈┈┈┈┈┈┈┈┈┈┈┈┈
-│➯‣❏ *✨*NOMBRE:* ${name}
-│➯‣❏ *💎Diamantes:* ${global.db.data.users[who].limit}💎
-│➯‣❏ *💵dolares* ${global.db.data.users[who].joincount}
-│➯‣❏ *♦️ʜᴀᴅᴇs-ᴄᴏɪɴs* ${global.db.data.users[who].money}
-│➯‣❏ *🧿Exp* ${global.db.data.users[who].exp}
-╰───────────────╯
-🍁᭢━━━━━━━━━᭥🍁᭢
+│💰 *《《BALANCE》》* 
+│┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+│🧑🏻‍💻 *NOMBRE:* ${name}
+│💎 *DIAMANTES:* ${global.db.data.users[who].limit}💎
+│💸 *DOLARES* ${global.db.data.users[who].joincount}
+│🕶 *DROID-COINS* ${global.db.data.users[who].money}
+│📝 *Exp* ${global.db.data.users[who].exp}
+╰✩⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆
 ╭「➻❥𝐃𝐑𝐎𝐈𝐃-8-𝐌𝐃➻❥」
-│➯‣ 📝*𝙽𝙾𝚃𝙰:*
-│➯‣ *para ir ala tienda y comprar Diamantes usa el comando*
-│➯‣❏ *.shop*
-╰───────────────╯`)
+│ *.shop* [Tienda]
+╰──────────────╯`)
 }
 handler.help = ['bal']
 handler.tags = ['xp']
