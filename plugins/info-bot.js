7380371bc093c80f24944cf66893bdebee2ecd13
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-23 09:48:49 -0600

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ let vn = './Audios/bot.mp3'
 let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) { 
 conn.sendPresenceUpdate('recording', m.chat)   
-await m.reply(`*𝙷𝙾𝙻𝙰, ¿𝙲𝙾𝙼𝙾 𝚃𝙴 𝙿𝚄𝙴𝙳𝙾 𝙰𝚈𝚄𝙳𝙰𝚁?*\n\n *HELLO, ¿HOW CAN I HELP YOU?* `)
+await m.reply(`*HOLA, CÓMO TE PUEDO AYUDAR?*\n\n *HELLO, ¿HOW CAN I HELP YOU?* `)
 conn.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', seconds: '4556', ptt: true }, { quoted: m })
 }
 return !0
