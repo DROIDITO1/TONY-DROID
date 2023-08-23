5ad2aed1185b6285f95899bcba1f92547b879fcc
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-23 00:44:06 -0600

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -6,7 +6,7 @@ else who = m.chat
 let users = global.db.data.users
 users[who].banned = true
 m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙿𝙾𝙳𝚁𝙰 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙷𝙰𝚂𝚃𝙰 𝚀𝚄𝙴 𝚂𝙴𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]THE USER WAS SUCCESSFULLY BANNED\n THE USER WILL NOT BE ABLE TO USE THE BOT UNTIL IT IS UNBANED*')    }
-handler.command = /^banuser$/i
+handler.command = /^ban$/i
 handler.rowner = true
 handler.register = true
 export default handler
