11401a12272233ed902b6e51f657d4be60a5474f
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-22 23:49:34 -0600

    Actualizar gc-warn.js

--- plugins/gc-warn.js
+++ plugins/gc-warn.js
@@ -1,1 +1,54 @@
-const _0x1504b4=_0x4aa7;function _0x4aa7(_0x4e898d,_0x1366a6){const _0x3b407d=_0x3b40();return _0x4aa7=function(_0x4aa779,_0x528f41){_0x4aa779=_0x4aa779-0x13c;let _0x1c88d3=_0x3b407d[_0x4aa779];return _0x1c88d3;},_0x4aa7(_0x4e898d,_0x1366a6);}(function(_0x18429f,_0x1325b2){const _0x145d1e=_0x4aa7,_0x3e0765=_0x18429f();while(!![]){try{const _0xac910e=parseInt(_0x145d1e(0x140))/0x1+parseInt(_0x145d1e(0x15e))/0x2+parseInt(_0x145d1e(0x142))/0x3*(-parseInt(_0x145d1e(0x159))/0x4)+-parseInt(_0x145d1e(0x13c))/0x5+-parseInt(_0x145d1e(0x150))/0x6+parseInt(_0x145d1e(0x156))/0x7+parseInt(_0x145d1e(0x144))/0x8*(-parseInt(_0x145d1e(0x151))/0x9);if(_0xac910e===_0x1325b2)break;else _0x3e0765['push'](_0x3e0765['shift']());}catch(_0x2dad1e){_0x3e0765['push'](_0x3e0765['shift']());}}}(_0x3b40,0x5848e));let war=global[_0x1504b4(0x15a)],handler=async(_0xaa2652,{conn:_0xe8d1da,text:_0x5caf20,args:_0x348db6,groupMetadata:_0x88a761,usedPrefix:_0x23674c,command:_0x79695b})=>{const _0xf91fc9=_0x1504b4;let _0x577097;if(_0xaa2652['isGroup'])_0x577097=_0xaa2652['mentionedJid'][0x0]?_0xaa2652[_0xf91fc9(0x15f)][0x0]:_0xaa2652[_0xf91fc9(0x154)]?_0xaa2652['quoted'][_0xf91fc9(0x146)]:![];else _0x577097=_0xaa2652[_0xf91fc9(0x15c)];if(!_0x577097)throw _0xf91fc9(0x143)+(_0x23674c+_0x79695b)+'\x20@user';if(!(_0x577097 in global['db'][_0xf91fc9(0x153)][_0xf91fc9(0x157)]))throw _0xf91fc9(0x14e);let _0xd66ece=_0xe8d1da[_0xf91fc9(0x13f)](_0xaa2652[_0xf91fc9(0x146)]),_0x1995e1=global['db']['data']['users'][_0x577097][_0xf91fc9(0x158)];if(_0x1995e1<war)global['db'][_0xf91fc9(0x153)][_0xf91fc9(0x157)][_0x577097][_0xf91fc9(0x158)]+=0x1,_0xaa2652[_0xf91fc9(0x148)]('\x0a╭•⋆҈͜͡.➣❂ᵈʳᵒⁱᵈⁱᵗᵒ\x20ᵇᵒᵗ❂◄⋆҈͜͡•╮\x0a┃😈\x20Usuario\x20Advertido\x0a┃\x0a┃\x20Admin:\x20'+_0xd66ece+_0xf91fc9(0x155)+_0x577097[_0xf91fc9(0x14f)]`@`[0x0]+_0xf91fc9(0x14b)+(_0x1995e1+0x1)+'/'+war+_0xf91fc9(0x13d)+_0x5caf20,null,{'mentions':[_0x577097]}),_0xaa2652[_0xf91fc9(0x148)](_0xf91fc9(0x13e)+(_0x1995e1+0x1)+'/'+war+_0xf91fc9(0x15d)+war+_0xf91fc9(0x14d),_0x577097);else _0x1995e1==war&&(global['db'][_0xf91fc9(0x153)][_0xf91fc9(0x157)][_0x577097][_0xf91fc9(0x158)]=0x0,_0xaa2652[_0xf91fc9(0x148)]('🚯\x20Superaste\x20las\x20'+war+'\x20advertencias\x20serás\x20eliminado\x20😈'),await time(0xbb8),await _0xe8d1da[_0xf91fc9(0x15b)](_0xaa2652[_0xf91fc9(0x15c)],[_0x577097],_0xf91fc9(0x14c)),_0xaa2652[_0xf91fc9(0x148)](_0xf91fc9(0x149)+_0x88a761['subject']+'\x20fuiste\x20advertido\x20'+war+_0xf91fc9(0x152),_0x577097));};handler[_0x1504b4(0x14a)]=[_0x1504b4(0x161)],handler[_0x1504b4(0x141)]=['group'],handler[_0x1504b4(0x160)]=['warn'],handler[_0x1504b4(0x145)]=!![],handler['admin']=!![],handler[_0x1504b4(0x147)]=!![];function _0x3b40(){const _0x447d4f=['warn','1184728rUhVpU','maxwarn','groupParticipantsUpdate','chat','\x20\x0aSi\x20recibes\x20','1171650jBCJYr','mentionedJid','command','warn\x20@user','1072235bvtoJc','\x0a┃\x20Razon:\x20','\x0a╭•⋆҈͜͡.𝐓𝐎𝐍𝐘⋆𝐎𝐅𝐂⋆҈͜͡•╮\x0a⚠️\x20*DVERTENCIA\x20⚠️\x0aRecibiste\x20una\x20advertencia\x20de\x20un\x20admin\x0a\x0a᳆͜͡➣\x20Warns:\x20','getName','624628teKPVD','tags','6odlCbI','🧑🏻‍💻\x20Etiqueta\x20o\x20menciona\x20a\x20alguien\x0a\x0a☣️\x20Ejemplo\x20👉🏻\x20','2216slSRVd','group','sender','botAdmin','reply','🧑🏻‍💻\x20Eliminado\x20del\x20grupo\x20','help','\x0a┃\x20Warns:\x20','remove','\x20advertencias\x20serás\x20eliminado\x20automáticamente\x20del\x20grupo','🧑🏻‍💻\x20El\x20usuario\x20no\x20se\x20encuentra\x20en\x20mi\x20base\x20de\x20datos','split','3345636eQbeIB','2799KEcahq','\x20veces','data','quoted','\x0a┃\x20Usuario:\x20@','4212075WINBWA','users'];_0x3b40=function(){return _0x447d4f;};return _0x3b40();}export default handler;const time=async _0x2eecb9=>{return new Promise(_0x1dec92=>setTimeout(_0x1dec92,_0x2eecb9));};
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+  if (m.mentionedJid.includes(conn.user.jid)) return;
+  let pp = "./views/warn.jpg.png";
+  let who;
+  if (m.isGroup)
+    who = m.mentionedJid[0]
+      ? m.mentionedJid[0]
+      : m.quoted
+      ? m.quoted.sender
+      : text;
+  else who = m.chat;
+  let user = global.db.data.users[who];
+  let bot = global.db.data.settings[conn.user.jid] || {};
+  let dReason = "Sin motivo";
+  let msgtext = text || dReason;
+  let sdms = msgtext.replace(/@\d+-?\d* /g, "");
+  let warntext = `*🧑🏻‍💻 ETIQUETA A UNA PERSONA O RESPONDA A UN MENSAJE DEL GRUPO PARA DARLE UNA ADVERTENCIA AL USUARIO*\n\n*—◉ EJEMPLO:*\n*${
+    usedPrefix + command
+  } @${global.suittag}*`;
+  if (!who)
+    throw m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext) });
+  user.warn += 1;
+  await m.reply(
+    `${
+      user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`
+    } USUARIO ADVERTIDO 😈!\nMotivo: ${sdms}\n*ADVERTENCIAS ${
+      user.warn
+    }/3*`,
+    null,
+    { mentions: [who] },
+  );
+  if (user.warn >= 3) {
+    if (!bot.restrict)
+      return m.reply(
+        "*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*",
+      );
+    user.warn = 0;
+    await m.reply(
+      `TE LO ADVERTI VARIAS VECES!!\n*@${
+        who.split`@`[0]
+      }* SUPERASTE LAS *3* ADVERTENCIAS, SERAS ELIMINADO POR GAY 🧑🏻‍💻`,
+      null,
+      { mentions: [who] },
+    );
+    await conn.groupParticipantsUpdate(m.chat, [who], "remove");
+  }
+  return !1;
+};
+
+handler.command = /^(advertir|advertencia|warn|warning)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+export default handler;
