90ceadde34390e473fa8271a864f127d70e94088
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-08-30 19:34:17 -0600

    Actualizar Sticker-sticker.js

--- plugins/Sticker-sticker.js
+++ plugins/Sticker-sticker.js
@@ -39,7 +39,7 @@ if (!stiker) stiker = e
 } finally {
 if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
 
-else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾. 𝙽𝙾 𝙾𝙻𝚅𝙸𝙳𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]SORRY, I MADE AN ERROR, TRY AGAIN. DO NOT FORGET TO RESPOND TO A VIDEO IMAGE OR INSERT THE LINK OF AN IMAGE FINISHING .jpg OF WHICH IT WILL BE CONVERTED INTO A STICKER* '
+else throw '*👨🏻‍💻 LO siento, ocurrió un error, vuelve a intentarlo.  no olvide responder a un imagen/vídeo/gif*\n\n\n*🐥*'
 }}
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
