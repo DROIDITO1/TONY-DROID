10c028cc24a7c19bab29d3796bc13ae100a50955
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-01 10:51:29 -0600

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -6,10 +6,10 @@ else who = m.chat
 if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]ENTER THE @tag OF SOME USER*'
 let users = global.db.data.users
 users[who].banned = false
-conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝚈𝙰 𝙿𝚄𝙴𝙳𝙴 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃*\n\n\n*[❗𝐈𝐍𝐅𝐎❗]THE USER WAS SUCCESSFULLY FADED\n THE USER CAN NOW USE THE BOT*`, m)
+conn.reply(m.chat, `*👨🏻‍💻 Desbaneado*\n*—◉El usuario ya puede usarme*`, m)
 }
 handler.help = ['unban']
-handler.tags = ['owner']
+handler.tags = ['admin']
 handler.command = /^unban$/i
 handler.admin = true
 handler.register = true
