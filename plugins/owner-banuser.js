21ace4a9a341ae3e00753fc7aeebbc32d92671c2
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-01 11:06:24 -0600

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let BANtext = `*👨🏻‍💻 ETIQUETE ALA PERSONA O RESPONDA AL MENSAJE DEL USUARIO QUE DESEA BANEAR PARA QUE YA No PUEDA USARME*\n\n*—◉ EJEMPLO:*\n*${usedPrefix + command} @${global.suittag}*\n\n\n[❗]*TAG A PERSON OR REPLY TO A MESSAGE SENT BY THE USER YOU WANT TO BAN EXAMPLE:*\n*${usedPrefix + command} @${global.suittag}*`
+let BANtext = `*👨🏻‍💻 ETIQUETE ALA PERSONA O RESPONDA AL MENSAJE DEL USUARIO QUE DESEA BANEAR PARA QUE YA No PUEDA USARME*\n\n*—◉ EJEMPLO:*\n*${usedPrefix + command} @${global.suittag}*`
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 else who = m.chat
