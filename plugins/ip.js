b8f14c1a5b85293d889761801bcdd330d6c508ba
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-03 23:46:35 -0600

    Actualizar ip.js

--- plugins/ip.js
+++ plugins/ip.js
@@ -1,5 +1,5 @@
-const free = 250 
- const prem = 500 
+const free = 500 
+ const prem = 600 
  let handler = async (m, { isPrems }) => { 
    let time = global.db.data.users[m.sender].lastclaim + 86400000 
    if (new Date - global.db.data.users[m.sender].lastclaim < 86400000) throw `🎁 *Ya recogiste tu recompensa diaria*\n\n🕚 Vuelve en *${msToTime(time - new Date())}* ` 
@@ -13,9 +13,9 @@ const free = 250
  ╰───────────────╯`) 
    global.db.data.users[m.sender].lastclaim = new Date * 1 
  } 
- handler.help = ['daily2'] 
+ handler.help = ['daily'] 
  handler.tags = ['limit'] 
- handler.command = ['daily2', 'claim2']  
+ handler.command = ['daily', 'claim']  
  handler.register = true 
  export default handler 
   
@@ -27,7 +27,7 @@ const free = 250
      minutes = Math.floor((duration / (1000 * 60)) % 60), 
      hours = Math.floor((duration / (1000 * 60 * 60)) % 12) 
   
-   hours = (hours < 19) ? "0" + hours : hours 
+   hours = (hours < 12) ? "0" + hours : hours 
    minutes = (minutes < 10) ? "0" + minutes : minutes 
    seconds = (seconds < 10) ? "0" + seconds : seconds 
   
