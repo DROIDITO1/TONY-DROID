d4e2112724771f7a43bc0dc7ad2f6794bc351b25
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-05 23:23:36 -0600

    Actualizar Anti-links2.js

--- plugins/Anti-links2.js
+++ plugins/Anti-links2.js
@@ -1,39 +1,27 @@
-// manden porno test 
-let linkRegex = /https:/i
-export async function before(m, { isAdmin, isBotAdmin, text }) {
-if (m.isBaileys && m.fromMe)
-return !0
-if (!m.isGroup) return !1
-let chat = global.db.data.chats[m.chat]
-let delet = m.key.participant
-let bang = m.key.id
-let bot = global.db.data.settings[this.user.jid] || {}
-let user = `@${m.sender.split`@`[0]}`
-const isGroupLink = linkRegex.exec(m.text)
-if (chat.antiLink2 && isGroupLink && !isAdmin) {
-if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-const linkThisGroup2 = `.imagen porno`
-const linkThisGroup3 = `.imagen xxx`
-const linkThisGroup3 = `.imagen pene`
-const linkThisGroup3 = `.imagen nepe`
-const linkThisGroup3 = `.imagen rule`
-const linkThisGroup3 = `.imagen rule34`
-const linkThisGroup3 = `.imagen desnuda`
-const linkThisGroup3 = `.imagen nopor`
-const linkThisGroup3 = `.imagen vagina`
-if (m.text.includes(linkThisGroup)) return !0
-if (m.text.includes(linkThisGroup2)) return !0
-if (m.text.includes(linkThisGroup3)) return !0
-}    
-await this.sendMessage(m.chat, { text: `*「 ANTI VIRGOS 」*\n*ESTÁ PROHIBIDO PEDIRME COSAS QUE VAN CONTRA LAS REGLAS  ${user} SERÁS ELIMINAD POR PENDEJO...!!*`, mentions: [m.sender] })  
-if (!isBotAdmin) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*')
-//await conn.sendButton(m.chat, `*「 ANTI VIRGOS 」*\n*🤓...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
-if (isBotAdmin && bot.restrict) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (responseb[0].status === "404") return
-} else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
-}
-return !0
-}
+const toxicRegex = /.imagen porno|.imagen xxx|.imagen desnuda|.imagen rule|.imagen pene|.imagen nepe|.imagen tetas|.imagen culo|.imagen sexo|.imagen rule34|.imagen vagina|.imagen panocha|.imagen mía Khalifa|/i 
+  
+ export async function before(m, { isAdmin, isBotAdmin, isOwner }) { 
+     if (m.isBaileys && m.fromMe) 
+         return !0 
+     if (!m.isGroup) 
+         return !1 
+     let user = global.db.data.users[m.sender] 
+     let chat = global.db.data.chats[m.chat] 
+     let bot = global.db.data.settings[this.user.jid] || {} 
+     const isToxic = toxicRegex.exec(m.text) 
+  
+     if (isVirgos && chat.antiVirgos && !isOwner && !isAdmin) { 
+        user.warn += 1 
+        if (!(user.remove >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.remove}/5* advertencia`, false, { mentions: [m.sender] }) 
+        if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, say the word (${isToxic}) is prohibited in this bot *${user.warn}/5* warning`, false, { mentions: [m.sender] }) 
+      } 
+  
+     if (user.kick >= 0) { 
+        user.remove = 0 
+        await m.reply(`*ʜᴏʟᴀ*@${m.sender.split`@`[0]},* SERÁS ELIMINADO POR PENDEJO`, false, { mentions: [m.sender] }) 
+        user.banned = true 
+        await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove') 
+        //await this.updateBlockStatus(m.sender, 'block') 
+     } 
+     return !1 
+ }
