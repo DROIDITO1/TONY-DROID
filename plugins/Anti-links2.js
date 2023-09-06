f1e6a75458561aa32e8d2573cb67134976e9185f
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-05 23:27:23 -0600

    Actualizar Anti-links2.js

--- plugins/Anti-links2.js
+++ plugins/Anti-links2.js
@@ -11,12 +11,12 @@ const toxicRegex = /.imagen porno|.imagen xxx|.imagen desnuda|.imagen rule|.imag
      const isToxic = toxicRegex.exec(m.text) 
   
      if (isVirgos && chat.antiVirgos && !isOwner && !isAdmin) { 
-        user.warn += 1 
-        if (!(user.remove >= 5)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.remove}/5* advertencia`, false, { mentions: [m.sender] }) 
-        if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, say the word (${isToxic}) is prohibited in this bot *${user.warn}/5* warning`, false, { mentions: [m.sender] }) 
+        user.remove += 0 
+        if (!(user.remove >= 0)) await m.reply(`${user.warn == 1 ? `Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, ESTÁ PROHIBIDO PEDIRME ESO *${user.remove}/0* A`, false, { mentions: [m.sender] }) 
+        if (!(user.warn >= 5)) await m.reply(`${user.remove == 1 ? `Hello *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`},  *${user.remove}/0* warning`, false, { mentions: [m.sender] }) 
       } 
   
-     if (user.kick >= 0) { 
+     if (user.remove >= 0) { 
         user.remove = 0 
         await m.reply(`*ʜᴏʟᴀ*@${m.sender.split`@`[0]},* SERÁS ELIMINADO POR PENDEJO`, false, { mentions: [m.sender] }) 
         user.banned = true 
