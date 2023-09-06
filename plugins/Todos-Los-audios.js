29fdb7a818b4a7557beff2ea2a07493862d8bad2
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-06 13:35:27 -0600

    Actualizar Todos-Los-audios.js

--- plugins/Todos-Los-audios.js
+++ plugins/Todos-Los-audios.js
@@ -8,7 +8,7 @@ this.sendPresenceUpdate('recording', m.chat)
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}    
 
 if (!chat.isBanned && chat.audios && m.text.match(/(Tony|+50258487658)/gi)) {
-let vn = './Audios/9d9e014dc98f993f9d0f1eb528e0b51d_06092023'
+let vn = './Audios/9d9e014dc98f993f9d0f1eb528e0b51d_06092023.m4a'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })} 
     
