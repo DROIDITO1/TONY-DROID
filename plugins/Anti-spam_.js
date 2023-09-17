2679f155ed545ff026285aa32a4e2d29be5e21f0
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-16 22:43:58 -0600

    Actualizar Anti-spam_.js

--- plugins/Anti-spam_.js
+++ plugins/Anti-spam_.js
@@ -1,45 +1,46 @@
 let handler = async (m, { conn, args, participants }) => {
-let users = Object.entries(global.db.data.users).map(([key, value]) => {
-return {...value, jid: key}})
-let sortedExp = users.map(toNumber('exp')).sort(sort('exp'))
-let sortedLim = users.map(toNumber('limit')).sort(sort('limit'))
-let sortedDola = users.map(toNumber('joincount')).sort(sort('joincount'))
-let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-let sortedRole = users.map(toNumber('role')).sort(sort('role'))
-let sortedRole2 = users.map(toNumber('role2')).sort(sort('role2'))
-let usersExp = sortedExp.map(enumGetKey)
-let usersDola = sortedDola.map(enumGetKey)
-let usersLim = sortedLim.map(enumGetKey)
-let usersLevel = sortedLevel.map(enumGetKey)
-let usersRole2 = sortedRole2.map(enumGetKey)
-let usersRole = sortedRole.map(enumGetKey)
-let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
-let text = `••••••••••••••••••
+    let users = Object.entries(global.db.data.users).map(([key, value]) => {
+        return { ...value, jid: key }
+    })
+    let sortedExp = users.map(toNumber('exp')).sort(sort('exp'))
+    let sortedLim = users.map(toNumber('limit')).sort(sort('limit'))
+    let sortedDola = users.map(toNumber('joincount')).sort(sort('joincount'))
+    let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
+    let sortedRole = users.map(toNumber('role')).sort(sort('role'))
+    let sortedRole2 = users.map(toNumber('role2')).sort(sort('role2'))
+    let usersExp = sortedExp.map(enumGetKey)
+    let usersDola = sortedDola.map(enumGetKey)
+    let usersLim = sortedLim.map(enumGetKey)
+    let usersLevel = sortedLevel.map(enumGetKey)
+    let usersRole2 = sortedRole2.map(enumGetKey)
+    let usersRole = sortedRole.map(enumGetKey)
+    let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
+    
+    let text = `••••••••••••••••••
 🧑🏻‍💻 *TABLA DE CLASIFICACION*
 ╰───────────────╯
 ╭「☆ *TOP ${len} DOLARES 💵* ☆」
 │➯Tú : *${usersDola.indexOf(m.sender) + 1}* de *${usersDola.length}*
-│➯${sortedDola.slice(0, len).map(({ jid, joincount }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${joincount} dolares*`).join`\n`}
+│➯${sortedDola.slice(0, len).map(({ jid, joincount, name }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${name} *${joincount} dolares*`).join`\n`}
 ─────────────────⋆
 ╭「☆ *TOP ${len} XP* ☆」
 │➯Tú : *${usersExp.indexOf(m.sender) + 1}* de *${usersExp.length}*
-│➯${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
+│➯${sortedExp.slice(0, len).map(({ jid, exp, name }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${name} *${exp} Exp*`).join`\n`}
 ─────────────────⋆
 ╭「☆ *TOP ${len} DIAMANTES* 💎 ☆」
 │➯Tú : *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-│➯${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Diamantes*`).join`\n`}
+│➯${sortedLim.slice(0, len).map(({ jid, limit, name }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${name} *${limit} Diamantes*`).join`\n`}
 ─────────────────⋆
 ╭「☆ *TOP ${len} NIVEL* ☆」
 │➯Tú : *${usersLevel.indexOf(m.sender) + 1}* de *${usersLevel.length}*
-│➯${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level}*`).join`\n`}
+│➯${sortedLevel.slice(0, len).map(({ jid, level, name }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${name} *Nivel ${level}*`).join`\n`}
 ─────────────────⋆`.trim()
-  m.reply(text, null, { mentions: conn.parseMention(text) })
+
+    m.reply(text, null, { mentions: conn.parseMention(text) })
 }
 handler.help = ['top']
 handler.tags = ['xp']
-handler.command = ['top2', 'lb'] 
-
-
+handler.command = ['top2', 'lb']
 
 handler.fail = null
 handler.exp = 0
@@ -47,17 +48,17 @@ handler.register = true
 export default handler
 
 function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
+    if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
+    else return (...args) => args[ascending & 1] - args[!ascending & 1]
 }
 
 function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
-  }
-  else return a => a === undefined ? _default : a
+    if (property) return (a, i, b) => {
+        return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
+    }
+    else return a => a === undefined ? _default : a
 }
 
 function enumGetKey(a) {
-  return a.jid
+    return a.jid
 }
